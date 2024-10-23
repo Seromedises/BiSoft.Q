b20acb83465b1eae613c5f662e1254ac26b2abc9
Giovanni Colucci <99266329+giocolucci@users.noreply.github.com>
2024-10-23 11:36:46 +0200

    Delete BiSoftQ_Class directory

--- BiSoftQ_Class/@Bellows/PushForce.m
+++
@@ -1,5 +0,0 @@
-function F_push = PushForce(obj,p)
-
-F_push = -p*pi*obj.Rib^2;
-
-end
--- BiSoftQ_Class/sincc.m
+++
@@ -1,7 +0,0 @@
-function y = sincc(x)
-% normalized sinc function, sin(pi*x)/(pi*x), no checks on the input
-%
-% y = sincc(x)
-y = sin(pi*x)./(pi*x);
-y(x==0) = 1;
-end
